c60a2f2089f72b3794f03b03aa268c3811a1ee0d
npazosmendez <npazosmendez@gmail.com>
2017-06-19 22:33:31 -0300

    tarea A M se mueve locamente

--- tareaAM.c
+++ tareaAM.c
@@ -7,28 +7,52 @@
 #include "colors.h"
 #include "defines.h"
 #include "game.h"
-#include "syscall.h" 
+#include "syscall.h"
 
 void task() {
-    syscall_mover(ADE);
-    syscall_mover(ADE);
-    /* Tarea */
-    /*int i;
-    for(i=0;i<10;i++) {
-    //breakpoint();
+    int i;
+    unsigned long* p = (unsigned long*) (0x8000000+(2<<13));
+    while(1){
+        for(i=0; i<5; i++){
+            *p = 0;
+            p++;
+            syscall_mover(ADE);
+            syscall_mover(ADE);
+            syscall_mover(DER);
+        }
+        for(i=0; i<5; i++){
+            *p = 0;
+            p++;
+            syscall_mover(DER);
+            syscall_mover(ATR);
+            syscall_mover(IZQ);
+            syscall_mover(IZQ);
+            syscall_mover(ADE);
+            syscall_mover(ADE);
+            syscall_mover(ADE);
+        }
+        for(i=0; i<5; i++){
+            *p = 0;
+            p++;
+            syscall_mover(ADE);
+            syscall_mover(DER);
+            syscall_mover(ATR);
+            syscall_mover(ADE);
+            syscall_mover(IZQ);
+            syscall_mover(ATR);
+            syscall_mover(ADE);
+            syscall_mover(IZQ);
+        }
+        for(i=0; i<5; i++){
+            *p = 0;
+            p++;
+            syscall_mover(ADE);
+            syscall_mover(ADE);
+            syscall_mover(DER);
+            syscall_mover(DER);
+            syscall_mover(DER);
+        }
     }
-    for(i=0;i<10;i++) {
-    syscall_mover(DER); //breakpoint();
-    }
-    for(i=0;i<10;i++) {
-    syscall_mover(ADE); //breakpoint();
-    }
-    for(i=0;i<10;i++) {
-    syscall_mover(IZQ); //breakpoint();
-    }
-    for(i=0;i<10;i++) {
-    syscall_mover(ADE); //breakpoint();
-    }*/
-    
+
    while(1) { __asm __volatile("mov $2, %%eax":::"eax"); }
 }
